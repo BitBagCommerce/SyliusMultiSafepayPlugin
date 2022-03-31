a49a3fc98c4211b0a04420a65e00c0df67746564
kuba-end <kostek660i@gmail.com>
2022-03-31 08:02:51 +0200

    Add support for Sylius 1.11 and drop for 1.8

---
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+];
