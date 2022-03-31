10b2a772289bef4309a3dcb232af7a8478f34857
kuba-end <kostek660i@gmail.com>
2022-03-31 16:01:20 +0200

    Add babdev/pagerfanta-bundle

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -55,4 +55,5 @@ return [
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
     Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
